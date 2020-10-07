9d66e58753c09590b899f078880610707a73c10c
ozzaynetworks <ozzaynetworks@gmail.com>
2020-10-07 18:36:16 +0300

    map.css'

--- js/mapping-data.js
+++ js/mapping-data.js
@@ -398,6 +398,10 @@
 
                     if(theGroup=="approved"){
                         alert("approved application");
+
+                        $('#car-info').removeClass('left-100').siblings().addClass('left-100');
+                        $('.main-map-container .ma-backdrop').removeClass('d-none');
+                        $(".content, .header").append('<div class="ma-backdrop" data-ma-action="aside-close" data-ma-target=' + e + " />");
                     }
 
                     
